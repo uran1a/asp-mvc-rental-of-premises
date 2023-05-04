4cff2702cf03e609e5109e0b10778f8d3562ec50
uran1a <tydom24@gmail.com>
2023-05-04 21:48:58 +0300

    add UI authorization

---
+++ RentalOfPremises/Views/Account/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RentalOfPremises.Views.Account
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- RentalOfPremises/wwwroot/js/main.js
+++ RentalOfPremises/wwwroot/js/main.js
@@ -1,51 +1,275 @@
-/*
-	Minimaxing by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
+    "use strict";
 
-	var $window = $(window),
-		$body = $('body');
+    /*****************************
+     * Commons Variables
+     *****************************/
+    var $window = $(window),
+        $body = $('body');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+    /****************************
+     * Sticky Menu
+     *****************************/
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll < 100) {
+            $(".sticky-header").removeClass("sticky");
+        } else {
+            $(".sticky-header").addClass("sticky");
+        }
+    });
 
-	// Nav.
+    /***************************
+	Humberger Main menu
+	***************************/
+    // Add slideDown animation to Bootstrap dropdown when expanding.
+    $('.dropdown').on('show.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
+    // Add slideUp animation to Bootstrap dropdown when collapsing.
+    $('.dropdown').on('hide.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
 
-		// Navigation Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+    /**************************
+     * Offcanvas: Menu Content
+     **************************/
+    function mobileOffCanvasMenu() {
+        var $offCanvasNav = $('.offcanvas-menu'),
+            $offCanvasNavSubMenu = $offCanvasNav.find('.mobile-sub-menu');
+
+        /*Add Toggle Button With Off Canvas Sub Menu*/
+        $offCanvasNavSubMenu.parent().prepend('<div class="offcanvas-menu-expand"></div>');
+
+        /*Category Sub Menu Toggle*/
+        $offCanvasNav.on('click', 'li a, .offcanvas-menu-expand', function(e) {
+            var $this = $(this);
+            if ($this.attr('href') === '#' || $this.hasClass('offcanvas-menu-expand')) {
+                e.preventDefault();
+                if ($this.siblings('ul:visible').length) {
+                    $this.parent('li').removeClass('active');
+                    $this.siblings('ul').slideUp();
+                    $this.parent('li').find('li').removeClass('active');
+                    $this.parent('li').find('ul:visible').slideUp();
+                } else {
+                    $this.parent('li').addClass('active');
+                    $this.closest('li').siblings('li').removeClass('active').find('li').removeClass('active');
+                    $this.closest('li').siblings('li').find('ul:visible').slideUp();
+                    $this.siblings('ul').slideDown();
+                }
+            }
+        });
+    }
+    mobileOffCanvasMenu();
+
+    /************************************************
+     * Nice Select
+     ***********************************************/
+    $('select').niceSelect();
+
+
+    /*************************
+     *   Hero Slider Active
+     **************************/
+    var heroSlider = new Swiper('.hero-slider .swiper-container', {
+        slidesPerView: 1,
+        speed: 5500,
+        effect: 'fade',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+       
+    });
+
+
+    /****************************************
+     *   Product Slider Active - 3 Grids 2 Rows
+     *****************************************/
+    var product_slider_3grids_2rows = new Swiper('.product-slider-3grids-2rows .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        speed: 1500,
+        slidesPerColumn: 2,
+        slidesPerColumnFill: 'row',
+        navigation: {
+            nextEl: '.center-slider-nav .button-next',
+            prevEl: '.center-slider-nav .button-prev',
+        },
+
+        breakpoints: {
+
+            0: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+            
+        }
+    });
+
+    /****************************************
+     *   Product Slider Active - 3 Grids 2 Rows
+     *****************************************/
+    var product_slider_3grids_1row = new Swiper('.product-slider-3grids-1row .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        speed: 1500,
+        navigation: {
+            nextEl: '.top-slider-buttons .button-next',
+            prevEl: '.top-slider-buttons .button-prev',
+        },
+
+        breakpoints: {
+
+            0: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+            
+        }
+    });
+
+    
+    /****************************************
+     *   company Logo
+     *****************************************/
+    var company_logo_slider = new Swiper('.company-slider .swiper-container', {
+        slidesPerView: 5,
+        autoplay: true,
+        speed: 1500,
+        loop: true,
+
+        breakpoints: {
+
+            0: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            992: {
+                slidesPerView: 4,
+            },
+            1200: {
+                slidesPerView: 5,
+            },
+            
+        }
+    });
+
+    /****************************************
+     *   Blog Feed - 2 Grids 1 Row
+     *****************************************/
+    var blog_feed_slider = new Swiper('.blog-feed-slide .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        speed: 1500,
+        loop: true,
+        autoplay: true,
+        breakpoints: {
+
+            0: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        }
+    });
+
+
+
+    /************************************************
+     * Video  Popup
+     ***********************************************/
+    $('.wave-btn').venobox(); 
+
+    /************************************************
+     * Price Range
+     ***********************************************/
+    $(".js-range-slider").ionRangeSlider({
+        skin: "round",
+        hide_min_max: true,    // show/hide MIN and MAX labels
+    });
+
+    /************************************************
+     * Product Quantity
+     ***********************************************/
+    $('.num-in span').click(function () {
+        var $input = $(this).parents('.num-block').find('input.in-num');
+      if($(this).hasClass('minus')) {
+        var count = parseFloat($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        if (count < 2) {
+          $(this).addClass('dis');
+        }
+        else {
+          $(this).removeClass('dis');
+        }
+        $input.val(count);
+      }
+      else {
+        var count = parseFloat($input.val()) + 1
+        $input.val(count);
+        if (count > 1) {
+          $(this).parents('.num-block').find(('.minus')).removeClass('dis');
+        }
+      }
+      
+      $input.change();
+      return false;
+    });
+
+
+      
+    /************************************************
+     * Scroll Top
+     ***********************************************/
+      function scrollToTop() {
+        var $scrollUp = $('#scroll-to-top'),
+          $lastScrollTop = 0,
+          $window = $(window);
+          $window.on('scroll', function () {
+          var st = $(this).scrollTop();
+            if (st > $lastScrollTop) {
+                $scrollUp.removeClass('show');
+            } else {
+              if ($window.scrollTop() > 120) {
+                $scrollUp.addClass('show');
+              } else {
+                $scrollUp.removeClass('show');
+              }
+            }
+            $lastScrollTop = st;
+        });
+        $scrollUp.on('click', function (evt) {
+          $('html, body').animate({scrollTop: 0}, 50);
+          evt.preventDefault();
+        });
+      }
+      scrollToTop();
 
 })(jQuery);
