6d193f43f040468b9bd1cab56521b30782efc0d1
uran1a <tydom24@gmail.com>
2023-05-15 03:21:12 +0300

    add admin page

---
+++ RentalOfPremises/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using RentalOfPremises.Models;
+using System.Diagnostics;
+using RentalOfPremises.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RentalOfPremises.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
